be32ffec7b4037c7e24f5d34d277d7560777f871
Kushal Thakar <kushalthakar555@gmail.com>
2026-07-12 15:04:25 -0400

    Adding UDPChatApp Project Specifications

---
+++ UDPChatApp/UDPChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UDPChatApp
+//
+//  Created by Kushal Thakar on 12/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UDPChatApp/UDPChatApp/UDPChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDPChatAppApp.swift
+//  UDPChatApp
+//
+//  Created by Kushal Thakar on 12/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UDPChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
